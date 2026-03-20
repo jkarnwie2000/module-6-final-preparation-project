5cd9db5c73b1f1481cebab8574fcb2934f422034
jkarnwie2000 <jkarnwie2000@yahoo.com>
2026-03-19 23:51:13 -0400

    test run

---
+++ src/pages/Posts.jsx
@@ -0,0 +1,53 @@
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom'
+import axios from 'axios';
+
+function Posts() {
+  const { id } = useParams();
+  const [posts, setPosts] = React.useState([]);
+  const [loading, setLoading] = React.useState(false);
+  const [searchId, setSearchId] = React.useState(id);
+
+  async function fetchPosts(userId) {
+  const { data } = await axios.get(`https://jsonplaceholder.typicode.com/posts?userId=${id}`);
+  setPosts(data);
+  setLoading(false);    
+    }
+  fetchPosts();   
+  
+  return (
+    <>
+  <div className="post__search">
+    <button>← Back</button>
+    <div className="post__search--container">
+      <label className="post__search--label">Search by Id</label>
+      <input
+        type="number"
+      />
+      <button>Enter</button>
+    </div>
+  </div>
+    {loading  
+    ? new Array(10).fill(0).map((_, index) => (
+  <div className="post" key={index}>
+  <div className="post__title">
+      <div className="post__title--skeleton"></div>
+  </div>
+  <div className="post__body">
+      <p className="post__body--skeleton"></p>
+  </div>
+  </div>
+      ))
+      : (
+    posts.map((post) => ( 
+    <div className="post" key={post.id}>
+    <div className="post__title">{post.title}</div>
+    <p className="post__body">{post.body}</p>
+  </div>  
+  ))    
+  )}  
+</>
+  );
+};
+
+export default Posts
